ce77a5258c483cd25fcf72becaa5698a5ca1491a
zahidupm <zahidhasanlimon111@gmail.com>
2022-12-16 23:23:32 +0600

    admin registration

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Posts') }}
+        </h2>
+    </x-slot>
+    <h3>Posts</h3>
+</x-app-layout>
